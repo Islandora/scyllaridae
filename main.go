121f47608dea1e16669c17b03eda658929c0a4f9
Joe Corall <jjc223@lehigh.edu>
2024-04-26 12:36:46 -0400

    rm direct event reads

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 }
 
 func MessageHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodPost && r.Method != http.MethodPut {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
