d59b6621e6df51a029dfbb0b4c285539c5d056c6
Joe Corall <jjc223@lehigh.edu>
2024-08-08 12:13:23 -0400

    fixup

--- ci/run.sh
+++ ci/run.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 cd "$SCRIPT_DIR"
-docker compose up -d 2>&1 > /dev/null
+docker compose up -d --quiet-pull 2>&1 > /dev/null
 
 docker exec ci-test-1 apk update 2>&1 > /dev/null
 docker exec ci-test-1 apk add bash curl file 2>&1 > /dev/null
