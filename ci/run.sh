e9d00cf7a783fe460490022bbaeba140ac000768
Joe Corall <jjc223@lehigh.edu>
2024-08-08 12:35:27 -0400

    Run as non-root user (#30)

---
+++ ci/run.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+cd "$SCRIPT_DIR"
+docker compose up -d --quiet-pull 2>&1 > /dev/null
+
+docker exec ci-test-1 apk update 2>&1 > /dev/null
+docker exec ci-test-1 apk add bash curl file 2>&1 > /dev/null
+docker exec ci-test-1 /test.sh
+echo $?
+
+docker compose down 2>&1 > /dev/null
+docker compose rm 2>&1 > /dev/null
