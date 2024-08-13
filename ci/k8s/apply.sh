85ae4babe86956a6b9c4bd93f13f74baf2408886
Joe Corall <jjc223@lehigh.edu>
2024-08-13 12:22:24 -0400

    fixup

--- ci/k8s/apply.sh
+++ ci/k8s/apply.sh
@@ -3,12 +3,20 @@
 set -eou pipefail
 
 TARGET="*.yaml"
-if [ -n "$1" ]; then
+if [ $# -eq 1 ]; then
+  if [ ! -f "$1" ]; then
+    echo "$1 doesn't exit"
+    exit 1
+  fi
+
   TARGET="$1"
 fi
+
+# Use eval to expand the wildcard in the TARGET variable
+eval "set -- $TARGET"
 
 sed -e "s|__DOMAIN__|$DOMAIN|" \
     -e "s|__DOCKER_REPOSITORY__|$DOCKER_REPOSITORY|" \
     -e "s|__KUBE_TLS_SECRET__|$KUBE_TLS_SECRET|" \
-    "$TARGET" \
+    "$@" \
 | kubectl apply -f -
