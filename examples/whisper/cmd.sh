3cd129134cbb46b99453ad0ff6dd20cf99ef1c22
Joe Corall <jjc223@lehigh.edu>
2024-09-19 14:45:37 -0400

    Download model outside container build

--- examples/whisper/cmd.sh
+++ examples/whisper/cmd.sh
@@ -4,12 +4,16 @@
 
 set -eou pipefail
 
+if [ ! -f /app/models/ggml-medium.en.bin ]; then
+  bash ./models/download-ggml-model.sh medium.en
+fi
+
 input_temp=$(mktemp /tmp/whisper-input-XXXXXX)
 
 cat > "$input_temp"
 
 /app/main \
-  -m /app/models/ggml-base.en.bin \
+  -m /app/models/ggml-medium.en.bin \
   --output-vtt \
   -f "$input_temp" \
   --output-file "$input_temp" > /dev/null 2>&1
