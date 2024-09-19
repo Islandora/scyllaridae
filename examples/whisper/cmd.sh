26ca6e3b3382ce26c2c7b34220cac1e3b477390f
Joe Corall <jjc223@lehigh.edu>
2024-09-19 09:23:14 -0400

    fixup cmd

--- examples/whisper/cmd.sh
+++ examples/whisper/cmd.sh
@@ -8,11 +8,12 @@ input_temp=$(mktemp /tmp/whisper-input-XXXXXX)
 
 cat > "$input_temp"
 
-libreoffice --headless --convert-to pdf "$input_temp" > /dev/null 2>&1
-
 /app/main \
   -m /app/models/ggml-base.en.bin \
   --output-vtt \
-  -f "$input_temp" 2>&1
+  -f "$input_temp" \
+  --output-file "$input_temp.vtt" > /dev/null 2>&1
 
-rm "$input_temp"
+cat "$input_temp.vtt"
+
+rm "$input_temp" "$input_temp.vtt"
