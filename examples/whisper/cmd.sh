a518fdcf3ca63e5d5137a6fe50ab7204af34151b
Joe Corall <jjc223@lehigh.edu>
2024-09-24 11:54:25 -0400

    Make sure we have a 16kHZ WAV file (#41)

--- examples/whisper/cmd.sh
+++ examples/whisper/cmd.sh
@@ -5,19 +5,32 @@
 set -eou pipefail
 
 if [ ! -f /app/models/ggml-medium.en.bin ]; then
-  bash ./models/download-ggml-model.sh medium.en
+  bash ./models/download-ggml-model.sh medium.en > /dev/null 2>&1
 fi
 
+# take stdin and buffer it into a temp file
 input_temp=$(mktemp /tmp/whisper-input-XXXXXX)
-
 cat > "$input_temp"
 
+# make sure we have a 16kHz WAV file
+output_file="${input_temp}_16khz.wav"
+ffmpeg -hide_banner -loglevel error -i "$input_temp" -ar 16000 -ac 1 "$output_file" 
+
+# generate the VTT file
 /app/main \
   -m /app/models/ggml-medium.en.bin \
   --output-vtt \
-  -f "$input_temp" \
-  --output-file "$input_temp" > /dev/null 2>&1
+  -f "$output_file" \
+  --output-file "$input_temp" > /dev/null 2>&1 || true
 
-cat "$input_temp.vtt"
+# make sure a VTT file was created
+STATUS=$(head -1  "$input_temp.vtt" | grep WEBVTT || echo "FAIL")
+if [ "$STATUS" != "FAIL" ]; then
+  cat "$input_temp.vtt"
+fi
 
-rm "$input_temp" "$input_temp.vtt"
+rm "$input_temp" "$input_temp.vtt" "$output_file"
+
+if [ "$STATUS" == "FAIL" ]; then
+  exit 1
+fi
