4a1d071ae641db89ab70d6099913e0f13f34d64f
Joe Corall <jjc223@lehigh.edu>
2024-12-07 09:02:13 -0500

    A little more port switching to help with host ports

--- examples/hls/cmd.sh
+++ examples/hls/cmd.sh
@@ -21,22 +21,24 @@ ffmpeg \
   -b:v 800k \
   -maxrate 800k \
   -bufsize 1200k \
-  -b:a 96k "$TMP_DIR/$NID.m3u8"
+  -b:a 96k "$TMP_DIR/$NID.m3u8" > /dev/null 2>&1
 
 if [ ! -f  "$TMP_DIR/$NID.m3u8" ]; then
   exit 1
 fi
 
-tar -czf "$TMP_DIR/hls.tar.gz" -C "$TMP_DIR" "$NID.m3u8" ./*.ts
+# shellcheck disable=SC2046
+tar -czf "$TMP_DIR/hls.tar.gz" -C "$TMP_DIR" "./$NID.m3u8" $(cd "$TMP_DIR" ; echo ./*.ts)
 
 BASE_URL=$(dirname "$NODE_URL" | xargs dirname)
-TID=$(curl "$BASE_URL/term_from_term_name?vocab=islandora_media_use&name=Service+File&_format=json" | jq '.[0].tid[0].value')
+TID=$(curl -s "$BASE_URL/term_from_term_name?vocab=islandora_media_use&name=Service+File&_format=json" | jq '.[0].tid[0].value')
 
-curl \
+curl -s \
+  -X PUT \
   -H "Authorization: $SCYLLARIDAE_AUTH" \
   -H "Content-Type: application/gzip" \
   -H "Content-Location: private://derivatives/hls/$NID/hls.tar.gz" \
-  -T "$TMP_DIR/hls.tar.gz" \
+  --data-binary "@$TMP_DIR/hls.tar.gz" \
   "$NODE_URL/media/file/$TID"
 
 rm -rf "$TMP_DIR"
