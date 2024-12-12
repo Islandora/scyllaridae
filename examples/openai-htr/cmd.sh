8ea171c1db36658ae7287c2d64df0ccc9d7ea607
Joe Corall <jjc223@lehigh.edu>
2024-12-12 13:07:04 -0500

    Change ocr prompt to instead enhance tesseract hOCR

--- examples/openai-htr/cmd.sh
+++ examples/openai-htr/cmd.sh
@@ -3,10 +3,26 @@
 set -eou pipefail
 
 TMP_DIR=$(mktemp -d)
+HOCR_URL="$1"
+DOMAIN=$(echo "$HOCR_URL"| awk -F/ '{print $1"//"$3}')
+# our hOCR filenames are the node ID
+NID=$(echo "$HOCR_URL" | xargs basename | awk -F '.hocr' '{print $1}')
 
+# take the base prompt and move it into place
+cp /app/prompt.txt "$TMP_DIR/prompt.txt"
+
+# the hOCR document is being streamed into this script
+# append the hOCR document into the prompt
+# since we're asking the LLM to improve the hOCR doc
+cat >> "$TMP_DIR/prompt.txt"
+CHAT_PROMPT=$(jq --null-input --rawfile rawstring "$TMP_DIR/prompt.txt" '$rawstring')
+
+# find the service file
+SERVICE_FILE_PATH=$(curl -s "$DOMAIN/node/$NID/service-file" | jq -r '.[0].file')
 # convert service file to jpg
-magick - "$TMP_DIR/img.jpg"
+curl -s "${DOMAIN}${SERVICE_FILE_PATH}" | magick - "$TMP_DIR/img.jpg"
 
+# chatgpt needs it base64 encoded
 BASE64_IMAGE=$(base64 -w 0 "$TMP_DIR/img.jpg")
 
 cat <<EOF > "$TMP_DIR/payload.json"
@@ -18,7 +34,7 @@ cat <<EOF > "$TMP_DIR/payload.json"
       "content": [
         {
           "type": "text",
-          "text": "$PROMPT"
+          "text": $CHAT_PROMPT
         },
         {
           "type": "image_url",
