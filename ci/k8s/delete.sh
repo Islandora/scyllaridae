e1a51fd346b65b869d372795cb5eaa0047eca619
Joe Corall <jjc223@lehigh.edu>
2024-12-20 06:09:25 -0500

    Have renovate detect examples/*/Dockerfile

---
+++ ci/k8s/delete.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# use sed to find/replace placeholders for nginx-ingress
+
+set -eou pipefail
+
+TARGET="ingress.yaml"
+if [ $# -eq 1 ]; then
+  if [ ! -f "$1" ]; then
+    echo "$1 doesn't exit"
+    exit 1
+  fi
+
+  TARGET="$1"
+fi
+
+sed -e "s|__DOMAIN__|$DOMAIN|" \
+    -e "s|__KUBE_TLS_SECRET__|$KUBE_TLS_SECRET|" \
+    "$@" \
+| kubectl delete -f -
