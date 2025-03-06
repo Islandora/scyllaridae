d9537070be02f9dcf854953c5729d0f5e3f99bf2
Joe Corall <jjc223@lehigh.edu>
2025-03-06 15:29:58 -0500

    Update cmd.sh

--- examples/cleanpdf/cmd.sh
+++ examples/cleanpdf/cmd.sh
@@ -4,10 +4,13 @@ set -eou pipefail
 
 PDF=$(mktemp)
 
+cleanup() {
+  rm -rf "$PDF"
+}
+trap cleanup EXIT
+
 cat > "$PDF"
 
 qpdf --replace-input --flatten-annotations=all "$PDF" > /dev/null 2>&1
 
 cat "$PDF"
-
-rm "$PDF"
